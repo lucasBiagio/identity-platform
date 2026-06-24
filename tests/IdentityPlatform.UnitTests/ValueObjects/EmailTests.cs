fe95e4222d9ae0043e2982f07a55e48a86ba7516
Lucas Biagio <lucas.fruhvald@gmail.com>
2026-06-24 17:10:59 -0300

    test(domain): add email value object tests

---
+++ tests/IdentityPlatform.UnitTests/ValueObjects/EmailTests.cs
@@ -0,0 +1,28 @@
+﻿using IdentityPlatform.Domain.ValueObjects;
+
+namespace IdentityPlatform.UnitTests.ValueObjects;
+
+public class EmailTests
+{
+    [Fact]
+    public void Should_Create_Email_When_Value_Is_Valid()
+    {
+        
+        var value = "lucas@email.com";
+        
+        var email = new Email(value);
+        
+        Assert.Equal(value, email.Value);
+    }
+
+    [Fact]
+    public void Should_Throw_Exception_When_Email_Is_Invalid()
+    {
+        // Arrange
+        var value = "email-invalido";
+
+        // Act & Assert
+        Assert.Throws<ArgumentException>(() =>
+            new Email(value));
+    }
+}
