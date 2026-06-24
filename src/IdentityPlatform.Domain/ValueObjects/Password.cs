b2663beabb7c8d6573ef28cf134cdea39a6d43e4
Lucas Biagio <lucas.fruhvald@gmail.com>
2026-06-24 17:14:12 -0300

    feat(domain): add password value object

---
+++ src/IdentityPlatform.Domain/ValueObjects/Password.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IdentityPlatform.Domain.ValueObjects;
+
+public sealed class Password
+{
+    public string Value { get; }
+
+    public Password(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            throw new ArgumentException(
+                "Password cannot be empty.",
+                nameof(value));
+
+        if (value.Length < 8)
+            throw new ArgumentException(
+                "Password must have at least 8 characters.",
+                nameof(value));
+
+        if (!value.Any(char.IsLetter))
+            throw new ArgumentException(
+                "Password must contain at least one letter.",
+                nameof(value));
+
+        if (!value.Any(char.IsDigit))
+            throw new ArgumentException(
+                "Password must contain at least one number.",
+                nameof(value));
+
+        Value = value;
+    }
+
+    public override string ToString()
+    {
+        return Value;
+    }
+}
---
+++ tests/IdentityPlatform.UnitTests/ValueObjects/PasswordTests.cs
@@ -0,0 +1,40 @@
+﻿using IdentityPlatform.Domain.ValueObjects;
+
+namespace IdentityPlatform.UnitTests.ValueObjects;
+
+public class PasswordTests
+{
+    [Fact]
+    public void Should_Create_Password_When_Value_Is_Valid()
+    {
+        // Arrange
+        var value = "MinhaSenha123";
+
+        // Act
+        var password = new Password(value);
+
+        // Assert
+        Assert.Equal(value, password.Value);
+    }
+
+    [Fact]
+    public void Should_Throw_Exception_When_Password_Is_Too_Short()
+    {
+        Assert.Throws<ArgumentException>(() =>
+            new Password("123"));
+    }
+
+    [Fact]
+    public void Should_Throw_Exception_When_Password_Has_No_Number()
+    {
+        Assert.Throws<ArgumentException>(() =>
+            new Password("MinhaSenha"));
+    }
+
+    [Fact]
+    public void Should_Throw_Exception_When_Password_Has_No_Letter()
+    {
+        Assert.Throws<ArgumentException>(() =>
+            new Password("12345678"));
+    }
+}
