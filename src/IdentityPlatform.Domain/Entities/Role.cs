45f45858908ce6e45f7bd00db5493efa4c00fc05
Lucas Biagio <lucas.fruhvald@gmail.com>
2026-06-24 16:50:35 -0300

    feat(domain): add permissions management to role

--- src/IdentityPlatform.Domain/Entities/Role.cs
+++ src/IdentityPlatform.Domain/Entities/Role.cs
@@ -8,8 +8,19 @@ namespace IdentityPlatform.Domain.Entities;
 
 public class Role : BaseEntity
 {
+    private readonly List<Permission> _permissions = [];
+
+    public IReadOnlyCollection<Permission> Permissions =>
+        _permissions.AsReadOnly();
     public string Name { get; private set; } = string.Empty;
 
+    public void AddPermission(Permission permission)
+    {
+        if (_permissions.Any(x => x.Id == permission.Id))
+            return;
+
+        _permissions.Add(permission);
+    }
     private Role()
     {
     }
