dd9fd8d1638645ffada4bedeabd16125490896a1
Lucas Biagio <lucas.fruhvald@gmail.com>
2026-06-24 16:45:49 -0300

    feat(domain): add role and permission entities

---
+++ src/IdentityPlatform.Domain/Entities/Permission.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using IdentityPlatform.Domain.Common;
+
+namespace IdentityPlatform.Domain.Entities;
+
+public class Permission : BaseEntity
+{
+    public string Name { get; private set; } = string.Empty;
+
+    private Permission()
+    {
+    }
+
+    public Permission(string name)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+    }
+}
---
+++ src/IdentityPlatform.Domain/Entities/Role.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using IdentityPlatform.Domain.Common;
+
+namespace IdentityPlatform.Domain.Entities;
+
+public class Role : BaseEntity
+{
+    public string Name { get; private set; } = string.Empty;
+
+    private Role()
+    {
+    }
+
+    public Role(string name)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+    }
+}
