19605aad077ac6917a01c5e378db9ff8741717aa
Lucas Biagio <lucas.fruhvald@gmail.com>
2026-06-25 09:35:08 -0300

    feat(application): add password hasher abstraction

---
+++ src/IdentityPlatform.Infrastructure/Repositories/InMemoryUserRepository.cs
@@ -0,0 +1,28 @@
+﻿using IdentityPlatform.Application.Abstractions;
+using IdentityPlatform.Domain.Entities;
+
+namespace IdentityPlatform.Infrastructure.Repositories;
+
+public class InMemoryUserRepository : IUserRepository
+{
+    private readonly List<User> _users = [];
+
+    public Task<bool> ExistsByEmailAsync(
+        string email,
+        CancellationToken cancellationToken = default)
+    {
+        var exists = _users.Any(x =>
+            x.Email.Value == email.ToLowerInvariant());
+
+        return Task.FromResult(exists);
+    }
+
+    public Task AddAsync(
+        User user,
+        CancellationToken cancellationToken = default)
+    {
+        _users.Add(user);
+
+        return Task.CompletedTask;
+    }
+}
