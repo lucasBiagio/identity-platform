6555f4818d8ed693f38a880f76023c917007cc36
Lucas Biagio <lucas.fruhvald@gmail.com>
2026-06-25 09:41:01 -0300

    feat(infrastructure): add ASP.NET password hasher

---
+++ src/IdentityPlatform.Infrastructure/Security/AspNetPasswordHasher.cs.cs
@@ -0,0 +1,24 @@
+﻿using IdentityPlatform.Application.Abstractions;
+using Microsoft.AspNetCore.Identity;
+
+namespace IdentityPlatform.Infrastructure.Security;
+
+public class AspNetPasswordHasher : IPasswordHasher
+{
+    private readonly PasswordHasher<object> _passwordHasher = new();
+
+    public string Hash(string password)
+    {
+        return _passwordHasher.HashPassword(new object(), password);
+    }
+
+    public bool Verify(string password, string passwordHash)
+    {
+        var result = _passwordHasher.VerifyHashedPassword(
+            new object(),
+            passwordHash,
+            password);
+
+        return result != PasswordVerificationResult.Failed;
+    }
+}
